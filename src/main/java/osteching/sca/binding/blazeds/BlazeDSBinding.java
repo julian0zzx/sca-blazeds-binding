0de77202fbf64560fb9f18569e3262794287e280
julian0zzx <julian0zzx@cca745a8-1dfa-11de-a027-6f5b8e30e268>
2010-01-20 09:20:51 +0000

    update

--- src/main/java/osteching/sca/binding/blazeds/BlazeDSBinding.java
+++ src/main/java/osteching/sca/binding/blazeds/BlazeDSBinding.java
@@ -2,6 +2,9 @@ package osteching.sca.binding.blazeds;
 
 import org.apache.tuscany.sca.assembly.*;
 
+/**
+ * @author julian0zzx@gmail.com
+ */
 public interface BlazeDSBinding extends Binding {
 
 }
---
+++ src/main/java/osteching/sca/binding/blazeds/broker/MessageBrokerProxy.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package osteching.sca.binding.blazeds.broker;
+
+import flex.messaging.FlexContext;
+import flex.messaging.MessageBroker;
+import flex.messaging.io.SerializationContext;
+import flex.messaging.io.TypeMarshallingContext;
+
+/**
+ * This class set up BlaseDS environment. 
+ * Most logic of this class comes from MessageBrokerServlet.
+ *  
+ * @author julian0zzx@gmail.com
+ */
+public class MessageBrokerProxy {
+    
+    static {
+        // allocate static thread local objects
+        MessageBroker.createThreadLocalObjects();
+        FlexContext.createThreadLocalObjects();
+        SerializationContext.createThreadLocalObjects();
+        TypeMarshallingContext.createThreadLocalObjects();
+    }
+    
+    /**
+     * Set up BlazeDS environment
+     */
+    public void init() {
+        ClassLoader loader = this.getClass().getClassLoader();
+        if (true) { // useContextClassLoader == true
+            loader = Thread.currentThread().getContextClassLoader();
+        }
+        
+    }
+    
+    public void destory() {
+        
+    }
+    
+    /** 
+     * Delegate to MessageBroker
+     */
+    public void invoke() {
+        
+    }
+}
