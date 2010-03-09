dfa99ceba507e798b239e1648fd1157c55aebfa9
julian0zzx <julian0zzx@cca745a8-1dfa-11de-a027-6f5b8e30e268>
2010-03-09 10:06:32 +0000

    add test skeleton

---
+++ trunk/src/test/java/osteching/sca/binding/blazeds/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package osteching.sca.binding.blazeds;
+
+/**
+ * @author julian0zzx@gmail.com
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+
+    }
+
+}
