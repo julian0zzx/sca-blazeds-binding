f82184e2877866343bcc6743d293d484970486d8
julian0zzx <julian0zzx@cca745a8-1dfa-11de-a027-6f5b8e30e268>
2010-03-15 15:32:20 +0000

    add test

--- trunk/src/main/java/osteching/sca/binding/blazeds/broker/MessageBrokerProxy.java
+++ trunk/src/main/java/osteching/sca/binding/blazeds/broker/MessageBrokerProxy.java
@@ -3,6 +3,17 @@
  */
 package osteching.sca.binding.blazeds.broker;
 
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Set;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -46,10 +57,11 @@ public final class MessageBrokerProxy {
      */
     public void init() {
         ClassLoader loader = getClassLoader();
+        ServletConfig mockServletConfig = getMockServletConfig();
         // Get the configuration manager
-        ConfigurationManager configManager = loadMessagingConfiguration();
+        ConfigurationManager configManager = loadMessagingConfiguration(mockServletConfig);
         // Load configuration
-        MessagingConfiguration config = configManager.getMessagingConfiguration(null);
+        MessagingConfiguration config = configManager.getMessagingConfiguration(mockServletConfig);
         // omit logging setting
         // Create broker.
         broker = config.createBroker("SCA-BlazeDS-Binding-Broker", loader);
@@ -114,11 +126,157 @@ public final class MessageBrokerProxy {
         return this.getClass().getClassLoader();
     }
 
-    private ConfigurationManager loadMessagingConfiguration() {
+    private ConfigurationManager loadMessagingConfiguration(ServletConfig config) {
         return new FlexConfigurationManager();
     }
 
     private void setupInternalPathResolver() {
+    }
+    
+    private ServletConfig getMockServletConfig() {
+        return new ServletConfig() {
+
+            @Override
+            public String getInitParameter(String arg0) {
+                return null;
+            }
+
+            @Override
+            public Enumeration getInitParameterNames() {
+                return null;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return new ServletContext() {
+
+                    @Override
+                    public Object getAttribute(String name) {
+                        return null;
+                    }
+
+                    @Override
+                    public Enumeration getAttributeNames() {
+                        return null;
+                    }
+
+                    @Override
+                    public ServletContext getContext(String uripath) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getContextPath() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getInitParameter(String name) {
+                        return null;
+                    }
+
+                    @Override
+                    public Enumeration getInitParameterNames() {
+                        return null;
+                    }
+
+                    @Override
+                    public int getMajorVersion() {
+                        return 0;
+                    }
+
+                    @Override
+                    public String getMimeType(String file) {
+                        return null;
+                    }
+
+                    @Override
+                    public int getMinorVersion() {
+                        return 0;
+                    }
+
+                    @Override
+                    public RequestDispatcher getNamedDispatcher(String name) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getRealPath(String path) {
+                        return null;
+                    }
+
+                    @Override
+                    public RequestDispatcher getRequestDispatcher(String path) {
+                        return null;
+                    }
+
+                    @Override
+                    public URL getResource(String path) throws MalformedURLException {
+                        return null;
+                    }
+
+                    @Override
+                    public InputStream getResourceAsStream(String path) {
+                        return null;
+                    }
+
+                    @Override
+                    public Set getResourcePaths(String path) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getServerInfo() {
+                        return null;
+                    }
+
+                    @Override
+                    public Servlet getServlet(String name) throws ServletException {
+                        return null;
+                    }
+
+                    @Override
+                    public String getServletContextName() {
+                        return null;
+                    }
+
+                    @Override
+                    public Enumeration getServletNames() {
+                        return null;
+                    }
+
+                    @Override
+                    public Enumeration getServlets() {
+                        return null;
+                    }
+
+                    @Override
+                    public void log(String msg) {
+                    }
+
+                    @Override
+                    public void log(Exception exception, String msg) {
+                    }
+
+                    @Override
+                    public void log(String message, Throwable throwable) {
+                    }
+
+                    @Override
+                    public void removeAttribute(String name) {
+                    }
+
+                    @Override
+                    public void setAttribute(String name, Object object) {
+                    }
+                };
+            }
+
+            @Override
+            public String getServletName() {
+                return null;
+            }
+        };
     }
     
     private void destroyThreadLocals() {
---
+++ trunk/src/test/java/osteching/sca/binding/blazeds/BuddyService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package osteching.sca.binding.blazeds;
+
+import java.util.List;
+
+/**
+ * @author julian0zzx@gmail.com
+ * 
+ */
+public interface BuddyService {
+    public Buddy add(Buddy buddy);
+
+    public List<Buddy> list();
+}
