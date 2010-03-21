7fba6a0ca898a38cd388b6ad63d5217f7f783b5c
julian0zzx <julian0zzx@cca745a8-1dfa-11de-a027-6f5b8e30e268>
2010-03-21 09:32:03 +0000

    test case, remove log

--- src/test/java/osteching/sca/binding/blazeds/BuddyServiceImpl.java
+++ src/test/java/osteching/sca/binding/blazeds/BuddyServiceImpl.java
@@ -3,11 +3,7 @@ package osteching.sca.binding.blazeds;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BuddyServiceImpl implements BuddyService {
-    final static Logger logger = LoggerFactory.getLogger(BuddyServiceImpl.class);
     private static List<Buddy> BUDDIES = new ArrayList<Buddy>();
     
     static {
@@ -18,9 +14,6 @@ public class BuddyServiceImpl implements BuddyService {
     
     @Override
     public Buddy add(Buddy buddy) {
-        if (logger.isDebugEnabled()) {
-            logger.debug("->-- add buddy into buddy collection - " + buddy);
-        }
         buddy.setId(BUDDIES.size());
         BUDDIES.add(buddy);
         return buddy;
@@ -28,9 +21,6 @@ public class BuddyServiceImpl implements BuddyService {
     
     @Override
     public List<Buddy> list() {
-        if (logger.isDebugEnabled()) {
-            logger.debug("->-- get buddies list - size is " + BUDDIES.size());
-        }
         return BUDDIES;
     }
     
