456a6f22b91353b91f5a2d5e2646cf9b219013ee
julian0zzx <julian0zzx@cca745a8-1dfa-11de-a027-6f5b8e30e268>
2010-03-22 15:55:26 +0000

    service name must be the same as @Service annotated interface.

--- trunk/src/test/java/osteching/sca/binding/blazeds/BuddyService.java
+++ trunk/src/test/java/osteching/sca/binding/blazeds/BuddyService.java
@@ -5,10 +5,15 @@ package osteching.sca.binding.blazeds;
 
 import java.util.List;
 
+import org.osoa.sca.annotations.Remotable;
+import org.osoa.sca.annotations.Service;
+
 /**
  * @author julian0zzx@gmail.com
  * 
  */
+@Remotable
+@Service
 public interface BuddyService {
     public Buddy add(Buddy buddy);
 
