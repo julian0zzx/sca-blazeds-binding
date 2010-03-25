b7ebbec2f329cb3977321c056cf8244f57bb093a
julian0zzx <julian0zzx@cca745a8-1dfa-11de-a027-6f5b8e30e268>
2010-03-25 08:42:33 +0000

    try to invoke pojo directly.

--- trunk/src/main/java/osteching/sca/binding/blazeds/broker/MessageBrokerServlet.java
+++ trunk/src/main/java/osteching/sca/binding/blazeds/broker/MessageBrokerServlet.java
@@ -3,9 +3,9 @@
  */
 package osteching.sca.binding.blazeds.broker;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
@@ -17,6 +17,17 @@ import org.apache.tuscany.sca.invocation.InvocationChain;
 import org.apache.tuscany.sca.invocation.Invoker;
 import org.apache.tuscany.sca.invocation.Message;
 import org.apache.tuscany.sca.invocation.MessageFactory;
+
+import flex.messaging.io.MessageDeserializer;
+import flex.messaging.io.MessageIOConstants;
+import flex.messaging.io.MessageSerializer;
+import flex.messaging.io.SerializationContext;
+import flex.messaging.io.amf.ActionContext;
+import flex.messaging.io.amf.ActionMessage;
+import flex.messaging.io.amf.MessageBody;
+import flex.messaging.messages.AcknowledgeMessage;
+import flex.messaging.messages.RemotingMessage;
+import flex.messaging.messages.SmallMessage;
 
 /**
  * 
@@ -47,26 +58,94 @@ public class MessageBrokerServlet extends HttpServlet {
 
             Message req = messageFactory.createMessage();
             Message res = serviceInvoker.invoke(req);
+            
             if (res.isFault()) {
                 Throwable e = (Throwable) res.getBody();
                 ((HttpServletResponse) response).sendError(
                                 HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
             }
 
-            // Write the response from the service implementation to the response
-            // output stream
-            InputStream is = (InputStream) res.getBody();
-            OutputStream os = response.getOutputStream();
-            byte[] buffer = new byte[2048];
-            for (;;) {
-                int n = is.read(buffer);
-                if (n <= 0)
-                    break;
-                os.write(buffer, 0, n);
-            }
-            os.flush();
-            os.close();
         }
     }
 
+    private void invokeBlazeDS(HttpServletRequest request, HttpServletResponse response)
+                    throws IOException {
+        ActionContext context = new ActionContext();
+        // Create an empty ActionMessage object to hold our response
+        context.setResponseMessage(new ActionMessage());
+        SerializationContext sc = SerializationContext.getSerializationContext();
+        // Deserialize the input stream into an "ActionMessage" object.
+        MessageDeserializer deserializer = sc.newMessageDeserializer();
+        // Set up the deserialization context
+        InputStream in = request.getInputStream();
+        deserializer.initialize(sc, in, null);
+        // record the length of the input stream for performance metrics
+        context.setDeserializedBytes(request.getContentLength());
+
+        try {
+            ActionMessage m = new ActionMessage();
+            context.setRequestMessage(m);
+            deserializer.readMessage(m, context);
+
+        } catch (Throwable t) {
+            // just ignore for test
+            t.printStackTrace();
+        } finally {
+            // Use the same ActionMessage version for the response
+            ActionMessage respMsg = context.getResponseMessage();
+            respMsg.setVersion(context.getVersion());
+        }
+
+        MessageBody inMessage = context.getRequestMessageBody();
+        MessageBody outMessage = context.getResponseMessageBody();
+        Message inMes = (Message) inMessage.getData();
+
+        String operationName = ((RemotingMessage) inMes).getOperation();
+        Object[] args = ((RemotingMessage) inMes).getParameters().toArray();
+        
+        Object result = null; // use reject to invoke target Class instance
+
+        AcknowledgeMessage ack = new AcknowledgeMessage();
+        ack.setBody(result);
+        ack.setCorrelationId(((RemotingMessage) inMes).getMessageId());
+        ack.setClientId(((RemotingMessage) inMes).getClientId());
+
+        // marshel ack
+        Object outData = convertToSmallMessage((flex.messaging.messages.Message) ack);
+
+        outMessage.setReplyMethod(MessageIOConstants.RESULT_METHOD);
+        outMessage.setData(outData);
+
+        try {
+            ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+            MessageSerializer serializer = sc.newMessageSerializer();
+            serializer.initialize(sc, outBuffer, null);
+            serializer.writeMessage(context.getResponseMessage());
+
+            // keep track of serializes bytes for performance metrics
+            context.setSerializedBytes(outBuffer.size());
+            context.setResponseOutput(outBuffer);
+        } catch (Throwable t) {
+            // just ignore for test
+            t.printStackTrace();
+        }
+
+        ByteArrayOutputStream outBuffer = context.getResponseOutput();
+        response.setContentType("application/x-amf");
+        response.setContentLength(outBuffer.size());
+        outBuffer.writeTo(response.getOutputStream());
+        response.flushBuffer();
+    }
+
+    private flex.messaging.messages.Message convertToSmallMessage(
+                    flex.messaging.messages.Message message) {
+        if (message instanceof flex.messaging.messages.SmallMessage) {
+            flex.messaging.messages.Message smallMessage = ((flex.messaging.messages.SmallMessage) message)
+                            .getSmallMessage();
+            if (smallMessage != null)
+                message = smallMessage;
+        }
+
+        return message;
+    }
 }
